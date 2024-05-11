206fb0a7affa3bab964613d0c2ba1ed4b3156389
Ernest <ernestez12@gmail.com>
2024-05-11 14:25:23 +0100

    Login Text update

--- src/app/(authpages)/login/page.jsx
+++ src/app/(authpages)/login/page.jsx
@@ -108,7 +108,7 @@ const Login = () => {
                 type="submit"
                 className="w-full text-white bg-green hover:bg-light-green focus:ring-4 focus:outline-none focus:ring-green font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
-                Create an account
+                Login
               </button>
               <p className="text-sm font-light text-dark-gray">
                 Already have an account?
