880d041f64624b49472f4e54b7466b23006c3386
godwin <65357127+macqueen006@users.noreply.github.com>
2024-05-11 09:20:53 +0100

    modified the auth pages

--- src/app/(authpages)/otp/page.jsx
+++ src/app/(authpages)/otp/page.jsx
@@ -154,28 +154,28 @@ const VerifyOtp = () => {
             <div className="flex items-center justify-center gap-3">
               <input
                 type="text"
-                className="w-14 h-14 text-center text-2xl font-extrabold text-dark bg-light-gray border border-gray hover:border-gray appearance-none rounded p-4 outline-none focus:bg-white focus:border-green focus:ring-2 focus:ring-green"
+                className="w-14 h-14 text-center text-2xl font-extrabold text-dark bg-light-gray border border-transparent hover:border-gray appearance-none rounded p-4 outline-none focus:bg-white focus:border-green focus:ring-2 focus:ring-green"
                 pattern="[0-9]*"
                 onChange={handlePattern}
                 maxLength="1"
               />
               <input
                 type="text"
-                className="w-14 h-14 text-center text-2xl font-extrabold text-dark bg-light-gray border border-gray hover:border-gray appearance-none rounded p-4 outline-none focus:bg-white focus:border-green focus:ring-2 focus:ring-green"
+                className="w-14 h-14 text-center text-2xl font-extrabold text-dark bg-light-gray border border-transparent hover:border-gray appearance-none rounded p-4 outline-none focus:bg-white focus:border-green focus:ring-2 focus:ring-green"
                 pattern="[0-9]*"
                 onChange={handlePattern}
                 maxLength="1"
               />
               <input
                 type="text"
-                className="w-14 h-14 text-center text-2xl font-extrabold text-dark bg-light-gray border border-gray hover:border-gray appearance-none rounded p-4 outline-none focus:bg-white focus:border-green focus:ring-2 focus:ring-green"
+                className="w-14 h-14 text-center text-2xl font-extrabold text-dark bg-light-gray border border-transparent hover:border-gray appearance-none rounded p-4 outline-none focus:bg-white focus:border-green focus:ring-2 focus:ring-green"
                 pattern="[0-9]*"
                 onChange={handlePattern}
                 maxLength="1"
               />
               <input
                 type="text"
-                className="w-14 h-14 text-center text-2xl font-extrabold text-dark bg-light-gray border border-gray hover:border-gray appearance-none rounded p-4 outline-none focus:bg-white focus:border-green focus:ring-2 focus:ring-green"
+                className="w-14 h-14 text-center text-2xl font-extrabold text-dark bg-light-gray border border-transparent hover:border-gray appearance-none rounded p-4 outline-none focus:bg-white focus:border-green focus:ring-2 focus:ring-green"
                 pattern="[0-9]*"
                 onChange={handlePattern}
                 maxLength="1"
@@ -195,7 +195,7 @@ const VerifyOtp = () => {
             {`Didn't`} receive code?{" "}
             <a
               className="font-medium text-green hover:text-light-green"
-              href="/resend-otp"
+              href="#"
             >
               Resend otp?
             </a>
@@ -203,128 +203,6 @@ const VerifyOtp = () => {
         </div>
       </div>
     </section>
-
-    // <div className=" font-montserrat lg:px-48 md:py-6 p-10 bg-white md:bg-none  lg:h-screen">
-    //   <Link href="/">
-    //     <div className="flex justify-center md:block">
-    //       <p className=" bg-green text-white text-sm md:mb-2 w-fit px-3 py-2 mb-4 font-bold  ">
-    //         MADE IN NIGERIALOGO
-    //       </p>
-    //     </div>
-    //   </Link>
-    //   <div className=" md:mx-16 ">
-    //     <p className=" text-right text-base mb-2 text-black hidden md:block">
-    //       Already a member? <Link href="/login">Sign in</Link>
-    //     </p>
-
-    //     <div className="  min-h-[75vh] bg-white  md:shadow md:grid grid-cols-2">
-    //       <div
-    //         className="py-6 bg-cover bg-center px-7 font-bold text-xl hidden md:block "
-    //         style={styles}
-    //       >
-    //         <p className="text-green">Register With Us</p>
-    //         <p className="text-dark-gray w-[50%] mb-14 text-base">
-    //           Your One Stop Website
-    //         </p>
-    //         <p className="w-[50%] text-base mb-2">Enjoy Our Unlimited Offers</p>
-    //         <div className="bg-[08AB70] bg-green bg-opacity-[50%] pl-14 text-white text-base font-normal leading-10 py-5 w-[80%] ">
-    //           <ul>
-    //             <li>Unlimited Posting</li>
-    //             <li>Featured products</li>
-    //             <li>Available on Search Engine</li>
-    //           </ul>
-    //         </div>
-    //       </div>
-    //       <div className="pt-10 font-normal text-sm text-center ">
-    //         <p>Check your inbox for the confirmation email</p>
-    //         <p className="text-[#1F7CD1]">info@made-n-nigeria.com</p>
-    //         <div className="text-center mt-10 leading-9">
-    //           <p>Confirm the code sent to your inbox</p>
-    //           <form action="" onSubmit={formik.handleSubmit}>
-    //             <div className="flex justify-center my-4">
-    //               <div className="mr-3">
-    //                 <input
-    //                   type="text"
-    //                   value={formik.values.firstOTP}
-    //                   onChange={formik.handleChange}
-    //                   onBlur={formik.handleBlur}
-    //                   name="firstOTP"
-    //                   className=" outline-none h-['50px'] w-9 rounded bg-light-green text-center bg-opacity-[17%]   "
-    //                 />
-    //               </div>
-    //               <div className="mr-3">
-    //                 <input
-    //                   type="text"
-    //                   value={formik.values.secondOTP}
-    //                   onChange={formik.handleChange}
-    //                   onBlur={formik.handleBlur}
-    //                   name="secondOTP"
-    //                   className=" outline-none h-['50px'] w-9 rounded bg-light-green text-center bg-opacity-[17%]   "
-    //                 />
-    //               </div>
-    //               <div className="mr-3">
-    //                 <input
-    //                   type="text"
-    //                   value={formik.values.thirdOTP}
-    //                   onChange={formik.handleChange}
-    //                   onBlur={formik.handleBlur}
-    //                   name="thirdOTP"
-    //                   className=" outline-none h-['50px'] w-9 rounded bg-light-green text-center bg-opacity-[17%]   "
-    //                 />
-    //               </div>
-    //               <div className="mr-3">
-    //                 <input
-    //                   type="text"
-    //                   value={formik.values.fourthOTP}
-    //                   onChange={formik.handleChange}
-    //                   onBlur={formik.handleBlur}
-    //                   name="fourthOTP"
-    //                   className=" outline-none h-['50px'] w-9 rounded bg-light-green text-center bg-opacity-[17%]   "
-    //                 />
-    //               </div>
-
-    //               {/* #1F7CD1 */}
-    //             </div>
-    //             <button
-    //               type="submit"
-    //               className="py-3 font-bold rounded text-white bg-green w-[90%] lg:w-[60%] my-5"
-    //             >
-    //               Send
-    //             </button>
-    //           </form>
-    //           <p>Did not receive email? Check Spam</p>
-    //           <Link href={"/resend-otp"} className="text-[#1F7CD1]">
-    //             Resend OTP
-    //           </Link>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="text-center text-sm font-semibold my-4 hidden md:block">
-    //     <Link href={"#"} className=" hover:text-[#1F7CD1]">
-    //       About Us
-    //     </Link>
-    //     <span className="mx-4">|</span>
-    //     <Link href={"#"} className=" hover:text-[#1F7CD1]">
-    //       FAQ
-    //     </Link>
-
-    //     <span className="mx-4">|</span>
-    //     <Link href={"#"} className=" hover:text-[#1F7CD1]">
-    //       Help
-    //     </Link>
-
-    //     <span className="mx-4">|</span>
-    //     <Link href={"#"} className=" hover:text-[#1F7CD1]">
-    //       Complaint
-    //     </Link>
-
-    //     <span className="mx-4">|</span>
-    //     <Link href={"#"} className=" hover:text-[#1F7CD1]">
-    //       Contact
-    //     </Link>
-    //   </div>
-    // </div>
   );
 };
 
