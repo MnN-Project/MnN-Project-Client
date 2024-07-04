6feeb8062f294939bc46ddc36770ff68a147f19b
godwin <65357127+macqueen006@users.noreply.github.com>
2024-07-04 09:30:47 +0100

    Pricing page

--- src/app/admin/requests/page.jsx
+++ src/app/admin/requests/page.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+"use client";
 import AdminContainer from "@/components/container/AdminContainer";
 
 const Requests = () => {
-    return (
-        <AdminContainer>Requests</AdminContainer>
-    )
+    const data = [
+        {
+          name: "Matthew",
+          editTime: "Edited a minute ago",
+          editor: "Ernest MNN",
+          image:
+            "https://images.unsplash.com/photo-1719952167462-b78f82215119?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8",
+        },
+        {
+          name: "Liaison",
+          editTime: "Henry 30 minute ago",
+          editor: "Ernest MNN",
+          image:
+            "https://images.unsplash.com/photo-1719836667976-c13531c60b97?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D",
+        },
+        {
+          name: "Rhoda",
+          editTime: "Edited an hour ago",
+          editor: "Loretta MNN",
+          image:
+            "https://images.unsplash.com/photo-1719917525997-5a1be9bf7ce1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90b3MtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D",
+        },
+      ];
+  return (
+    <AdminContainer>
+      
+    </AdminContainer>
+  );
 };
 
 export default Requests;
